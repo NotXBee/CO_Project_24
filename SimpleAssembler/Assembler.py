aa4a5670f5681eec29e87c45bad2595f4f240ecb
Aditya Nuli <adityanuli@MacBook-Air-5.local>
2024-03-09 20:09:38 +0530

    Progressed in Assembler.py

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -21,12 +21,13 @@ with open(abs_path + "CO_Project_24/automatedTesting/tests/assembly/user_bin_s/o
             reg2 = text[3]
             if operation == 'sub' :
                 output += funct7(operation)
-                output += rs(reg1)
                 output += rs(reg2)
+                output += rs(reg1)
                 output += funct3(operation)
                 output += rs(dest)
-                output += opcode("r_type")
-                
+                output += opcode["r_type"]
+                print(output)
+                break
 
 
 
--- SimpleAssembler/utility.py
+++ SimpleAssembler/utility.py
@@ -2,12 +2,7 @@ opcode = {
     "r_type" : "0110011"
 }
 
-'''
-This first7 dictionary lists all the funct7 binary codes
-for the operations in rtype
-complete this one for all operations in rtype
-'''
-first7 = {
+f7 = {
     "add" : "0000000",
     "sub" : "0100000",
     "sll" : "0000000",
@@ -19,33 +14,61 @@ first7 = {
     "and" : "0000000",
 }
 
-'''
-Make this register address dictionary, you will understand by reading what I was doing.
-Just complete this
-'''
+f3 = {
+    "add" : "000",
+    "sub" : "000",
+    "sll" : "001",
+    "slt" : "010",
+    "sltu": "011",
+    "xor" : "100",
+    "srl" : "101",
+    "or"  : "110",
+    "and" : "111",
+}
+
+
 register_address = {     
        
-                    'zero': '00000','ra':'00001', 
-                    'sp': '00010', 'gp': '00011',
-                    'tp': '00100', 't0': '00101', 
-                    't1': '00110', 't2': '00111',
-                    's0': '01000', 'fp': '01000',
-                    's1': '01001', 'a0': '01010',
-                    'a1': '01011', 'a2': '01100', 
-                    'a3': '01101', 'a4': '01110',
-                    'a5': '01111', 'a6': '10000',
-                    'a7': '10001', 's2': '10010', 
-                    's3': '10011', 's4': '10100', 
-                    's5': '10101', 's6': '10110', 
-                    's7': '10111', 's8': '11000', 
-                    's9': '11001', 's10': '11010', 
-                    's11': '11011', 't3': '11100', 
-                    't4': '11101', 't5': '11110', 
-                    't6': '11111'
-                   }
+        'zero': '00000',
+        'ra':'00001',   
+        'sp': '00010', 
+        'gp': '00011',
+        'tp': '00100', 
+        't0': '00101', 
+        't1': '00110', 
+        't2': '00111',
+        's0': '01000', 
+        'fp': '01000',
+        's1': '01001', 
+        'a0': '01010',
+        'a1': '01011', 
+        'a2': '01100', 
+        'a3': '01101', 
+        'a4': '01110',
+        'a5': '01111', 
+        'a6': '10000',
+        'a7': '10001', 
+        's2': '10010', 
+        's3': '10011', 
+        's4': '10100', 
+        's5': '10101', 
+        's6': '10110', 
+        's7': '10111', 
+        's8': '11000', 
+        's9': '11001', 
+        's10': '11010', 
+        's11': '11011', 
+        't3': '11100', 
+        't4': '11101', 
+        't5': '11110', 
+        't6': '11111'
+}
 
 def funct7(operation) :
-    return first7[operation]
+    return f7[operation]
+
+def funct3(operation) :
+    return f3[operation]
 
 def rs(register) :
-    pass
+    return register_address[register]
