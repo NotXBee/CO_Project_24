d6085585640d683ac1fbbe227b27291d66fdd667
sanskargarg27 <162811073+sanskargarg27@users.noreply.github.com>
2024-04-17 10:27:55 +0530

    added some instructions

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -15,3 +15,36 @@ print("0b"+binary(pc),end=" ")
 for i in registers.values():
     print("0b"+binary(i),end=" ")
 print()
+
+if type(i) == "lw":
+    pc+=4
+    rs1 = i[-20:-15]
+    imm = twos_complement(i[-32:-20])
+    rd = i[-12:-7]
+    registers[rd] = memory[rs1 + imm]
+
+if type(i) == "addi":
+    pc+=4
+    rs = i[-20:-15]
+    imm = twos_complement(i[-32:-20])
+    rd = i[-12:-7]
+    registers[rd] = registers[rs] + imm
+
+if type(i) == "sltiu":
+    pc+=4
+    rs = i[-20:-15]
+    imm = twos_complement(i[-32:-20])
+    rd = i[-12:-7]
+    if(registers[rs] < imm):
+        registers[rd] = 1
+
+if type(i) == "jalr":
+    pc+=4
+    rs = i[-20:-15]
+    imm = twos_complement(i[-32:-20])
+    rd = i[-12:-7]
+
+    registers[rd] = pc
+    pc = registers[rs] + imm
+    if (pc%2):
+        pc -= 1
