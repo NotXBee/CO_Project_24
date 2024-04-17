c293a73ff19a6d020f45ce5b4f3c0000a7532a02
Aditya Nuli <adityanuli@MacBook-Air-5.local>
2024-04-17 18:20:24 +0530

    finishing touches

--- SimpleSimulator/temp.py
+++
@@ -1,9 +0,0 @@
-def binary(int, no_of_bits) :
-    if int >= 0 :
-        return format(int, f'0{no_of_bits}b')
-    
-    return format((1 << no_of_bits) + int, f'0{no_of_bits}b')
-
-
-x1 = 0b00000000000000000000000000000100
-print('0b' + binary(x1, 32))
