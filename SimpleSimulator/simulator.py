cb8b16726cbdfa2c4af27fe95fc889c94602619a
Aditya Nuli <adityanuli@MacBook-Air-5.local>
2024-04-17 12:13:38 +0530

    made some modifications

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -3,16 +3,78 @@ from helper import *
 with open("automatedTesting/tests/bin/simple/s_test1.txt","r") as f:
     txt = f.read().split("\n")
 
-i = txt[4]
+i = txt[10]
 
 # R type
-if type(i) == "add":
+if type(i) == "add": # verified
     rs2 = i[-25:-20]
     rs1 = i[-20:-15]
     rd = i[-12:-7]
     registers[rd] = registers[rs2] + registers[rs1]
     pc+=4
-    
+
+if type(i) == "sub" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    registers[rd] = registers[rs1] - registers[rs2]
+    pc += 4
+
+if type(i) == "sll" : # verified
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    temp = registers[rs2] % 32
+    registers[rd] = registers[rd]*(2**temp)
+    pc += 4
+
+if type(i) == "slt" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    if registers[rs1] < registers[rs2] :
+        registers[rd] = 1
+    pc += 4
+
+if type(i) == "sltu" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    if (unsigned(registers[rs1]) < unsigned(registers[rs2])) :
+        registers[rd] = 1
+    pc += 4
+
+if type(i) == "xor" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    registers[rd] = registers[rs1] ^ registers[rs2]
+    pc += 4
+
+if type(i) == "srl" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    temp = registers[rs2] % 32
+    registers[rd] = registers[rd]/(2**temp)
+    pc += 4
+
+if type(i) == "or" :
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    registers[rd] = registers[rs1] | registers[rs2]
+    pc += 4
+
+if type(i) == "and" : # verified
+    rs2 = i[-25:-20]
+    rs1 = i[-20:-15]
+    rd = i[-12:-7]
+    registers[rd] = registers[rs1] & registers[rs2]
+    pc += 4
+
+
+# B type
 if type(i) == "beq":
     imm = i[-32]+i[-8]+i[-31:-25]+i[-12:-8]
     rs2 = i[-25:-20]
@@ -70,43 +132,6 @@ if type(i) == "bltu":
     else:
         pc += 4
         
-if type(i) == "sub" :
-    rs2 = i[-25:-20]
-    rs1 = i[-20:-15]
-    rd = i[-12:-7]
-    registers[rd] = registers[rs1] - registers[rs2]
-    pc += 4
-
-if type(i) == "sll" :
-    rs2 = i[-25:-20]
-    rs1 = i[-20:-15]
-    rd = i[-12:-7]
-    temp = registers[rs2] % 32
-    registers[rd] = registers[rd]*(2**temp)
-    pc += 4
-
-if type(i) == "slt" :
-    rs2 = i[-25:-20]
-    rs1 = i[-20:-15]
-    rd = i[-12:-7]
-    if registers[rs1] < registers[rs2] :
-        registers[rd] = 1
-    pc += 4
-
-if type(i) == "sltu" :
-    rs2 = i[-25:-20]
-    rs1 = i[-20:-15]
-    rd = i[-12:-7]
-    if (unsigned(registers[rs1]) < unsigned(registers[rs2])) :
-        registers[rd] = 1
-    pc += 4
-
-if type(i) == "xor" :
-    rs2 = i[-25:-20]
-    rs1 = i[-20:-15]
-    rd = i[-12:-7]
-
-
 
 # I type
 if type(i) == "lw":
